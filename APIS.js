8c2ceab381c4b6a2c82691b9422d805fe13fb2e4
Ballesd <ballesterosj500@gmail.com>
2022-05-12 17:06:47 -0500

    Segundo grafo Bubble con sus respecetivos datos

--- APIS.js
+++ APIS.js
@@ -19,11 +19,8 @@ function grafica_scatter() {
                     indice_infancia.push(data_obtanined.ndice_deinfancia);
                     indice_juventud.push(data_obtanined.ndice_dejuventud);
                     indice_vejez.push(data_obtanined.ndice_de_vejez);
-
                 }
-
             });
-
             let trace1 = {
                 x: fecha,
                 y: indice_juventud,
@@ -67,7 +64,6 @@ function grafica_scatter() {
 
             let layout = {
                 title: "Indicadores demográficos Manizales 1985-2035",
-
                 xaxis:
                 {
                     title: "Año"
@@ -91,3 +87,105 @@ function grafica_scatter() {
         });
 }
 //fin función primer api
+
+//Inicio funcion Bubble segunda api acerca de las tasas y metodos de suicio por la población colombiana
+function grafica_bubble(){
+    let area = [];
+    let genero = [];
+    let edad = [];
+    let metodo = [];
+
+    let url = 'https://www.datos.gov.co/resource/gh4e-xuyg.json';
+    fetch(url)
+        .then(datos_obtenidos => datos_obtenidos.json())
+
+        .then(function (datos_obtenidos){
+            datos_obtenidos.forEach(function agregar(datos_obtenidos){
+              if(datos_obtenidos.area_del_hecho != undefined && datos_obtenidos.genero != undefined 
+                && datos_obtenidos.edad != undefined && datos_obtenidos.mecanismo_muerte != undefined){
+                   
+                    area.push(datos_obtenidos.area_del_hecho);
+                    genero.push(datos_obtenidos.genero);
+                    edad.push(datos_obtenidos.edad);
+                    metodo.push(datos_obtenidos.mecanismo_muerte);
+                }  
+            });
+            var traza1 = {
+                x: edad,
+                y: genero,
+                text: ['</br>tamaño: 20','<br>tamaño: 40', '<br>tamaño: 60', '<br>tamaño: 80', '<br>tamaño: 100', '</br>tamaño: 120'],
+                mode: 'markers',
+                name: 'Genero',
+                textposition: 'top center',
+                textfont: {
+                    family: 'Raleway, Times New Roman'
+                },
+                marker: {
+                    size: [200,400, 600, 800, 1000, 1200, 1400],
+                    sizeref: 0.3, //Tamaño del circulo
+                    sizemode: 'area',
+                    color: 'rgb(130,236,111)',
+                }
+            };
+            var traza2 = {
+                x: edad,
+                y: metodo,
+                text: ['</br>tamaño: 20','</br>tamaño: 40', '</br>tamaño: 60', '</br>tamaño: 80', '</br>tamaño: 100', '</br>tamaño: 120'],
+                mode: 'markers',
+                name: 'Método',
+                textposition: 'bottom center',
+                textfont: {
+                    family: 'Raleway, Times New Roman'
+                },
+                marker: {
+                    size: [200,400, 600, 800, 1000, 1200, 1400],
+                    sizeref: 0.3,
+                    sizemode: 'area',
+                    color: 'rgb(217,96,70)',
+                }
+            };
+            var traza3 = {
+                x: edad,
+                y: area,
+                text: ['</br>tamaño: 20','</br>tamaño: 40', '</br>tamaño: 60', '</br>tamaño: 80', '</br>tamaño: 100', '</br>tamaño: 120'],
+                mode: 'markers',
+                name: 'Lugar del hecho',
+                textposition: 'bottom center',
+                textfont: {
+                    family: 'Raleway, Times New Roman'
+                },
+                marker: {
+                  size: [200,400, 600, 800, 1000, 1200, 1400],
+                  sizeref: 0.3,
+                  sizemode: 'area',
+                  color: 'rgb(155,66,75)',
+                }
+            };
+              
+            var data = [traza1,traza2,traza3];
+              
+            var layout = {
+                title: 'Casos de suicidio en el país',
+                xaxis:
+                {
+                    title: "Edad"
+                },
+                showlegend: true,
+                height: 600,
+                width: 1200,
+                legend: {
+                    y: 0.6,
+                    yref: 'paper',
+                    font: {
+                        family: 'Arial Narrow',
+                        size: 16,
+                        color: 'grey-dark', 
+                    }
+                }
+            };
+              
+            Plotly.newPlot('Grafo2', data, layout);
+        })
+}
+
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ApisTaller
 Taller con herramientas javascript + potly + archivos en formato json 
-Juan Camilo Ballesteros Delgado
-Erik Palacio Castellanos
+Juan Camilo Ballesteros Delgado,
+Erik Palacio Castellanos,
 Danny Javier Vasquez 
